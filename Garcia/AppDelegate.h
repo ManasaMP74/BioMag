4eeb0e72a460b78edce3c06ff275e7ff0252b869
manasap <manasap@isadmins-Mac-mini.local>
2015-10-16 09:44:14 +0530

    Initial Commit

---
+++ Garcia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Garcia
+//
+//  Created by manasap on 16/10/15.
+//  Copyright (c) 2015 manasap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Garcia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Garcia
+//
+//  Created by manasap on 16/10/15.
+//  Copyright (c) 2015 manasap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
