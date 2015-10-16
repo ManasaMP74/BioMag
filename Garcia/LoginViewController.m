d445e6ea33b8b7aadbc3ffbcd93218d4d5d197e3
manasap <manasap@isadmins-Mac-mini.local>
2015-10-16 11:08:50 +0530

    c

--- Garcia/LoginViewController.m
+++ Garcia/LoginViewController.m
@@ -1,6 +1,9 @@
 #import "LoginViewController.h"
 
 @interface LoginViewController ()
+@property (strong, nonatomic) IBOutlet UITextField *userNameTf;
+@property (strong, nonatomic) IBOutlet UITextField *passwordTF;
+@property (strong, nonatomic) IBOutlet UIButton *signIn;
 
 @end
 
@@ -14,5 +17,11 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     
+}
+- (IBAction)signIn:(id)sender {
+    [self performSegueWithIdentifier:@"loginSucess" sender:nil];
+}
+-(void)placeHolder{
+
 }
 @end
