a62c8b2ef10aec3fb64225d8218563c421b06231
ManasaMP <manasap@vmokshagroup.com>
2016-04-14 12:53:10 +0530

    commit after warning fix

--- environment_preprocess.h
+++ environment_preprocess.h
@@ -5,4 +5,3 @@
 
 #define VM_ENVIRONMENT              DEV
 #define VM_ENVIRONMENT_LAST_COMMIT  9c51190
-#define VM_ENVIRONMENT_APP_NAME     File Doesn't Exist, Will Create: /Users/manasap/Desktop/BioMagneticWithVZoneURLjankins/Biomagnetism/environment.plist 01
