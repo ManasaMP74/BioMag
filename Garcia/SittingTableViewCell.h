bdb29c2b9f7f8aa20f48d6122c1696795beff1f5
ManasaMP <manasap@vmokshagroup.com>
2016-05-13 15:55:05 +0530

    c

--- Garcia/SittingTableViewCell.h
+++ Garcia/SittingTableViewCell.h
@@ -41,5 +41,6 @@
 @property (weak, nonatomic) IBOutlet UIButton *selectDeselectButton;
 @property (weak, nonatomic) IBOutlet UIImageView *expandButton;
 @property (weak, nonatomic) IBOutlet UIButton *completeSelectDeselectBtn;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *previousSittingCollectionViewHeight;
 
 @end
